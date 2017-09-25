9fed4043c03292c6a4823d63c257e62a987e78c4
Rob Emmerson <rob.emmerson@timeinc.com>
2017-09-25 13:02:28 +0100

    Moved setup stages to the setup.sh file, extraction can now be run multiple times

--- extract.sh
+++ extract.sh
@@ -6,23 +6,14 @@
 # Created: 21/09/2017
 
 
-if [ -e NTDS/NTDS.dit ] && [ -e NTDS/SYSTEM ] && [ -e NTDS/SAM ]
+if [ -e NTDS.export/datatable.4 ] && [ -e NTDS.export/link_table.7 ] && [ -e NTDS/SYSTEM ]
 then
-	echo "Extracting libesedb..."
-	tar xzf libesedb-experimental-20140406.tar.gz
-	cd libesedb-20140406
-	echo "Building libesedb..."
-	CFLAGS="-g -O2 -Wall -fgnu89-inline" ./configure --enable-static-executables >/dev/null 2>&1
-	make >/dev/null 2>&1
-	cd ..
-	echo "Preparing datafiles using esedbexport..."
-	./libesedb-20140406/esedbtools/esedbexport -t NTDS NTDS/NTDS.dit >/dev/null 2>&1
 	echo "Extracting hashes..."
 	python ntdsxtract/dsusers.py NTDS.export/datatable.4 NTDS.export/link_table.7 NTDS.export --syshive NTDS/SYSTEM --ntoutfile ../NTDS/extracted_hashes.txt --pwdformat ophc --passwordhashes --passwordhistory >/dev/null 2>&1
 	echo "Done!"
 else
 	echo ""
-	echo "Error: NTDS.dit, SYSTEM and SAM are not all present in the NTDS folder"
+	echo "Error: NTDS.export/datatable.4, NTDS.export/link_table.7 and NTDS/SYSTEM are not all present"
 	echo ""
 	echo ""
 fi
--- setup.sh
+++ setup.sh
@@ -4,4 +4,26 @@
 git submodule init
 git submodule update
 
+# Make 'extract.sh' executable
 chmod +x extract.sh
+
+# Check for the presence of the NTDS files
+if [ -e NTDS/NTDS.dit ] && [ -e NTDS/SYSTEM ] && [ -e NTDS/SAM ]
+then
+	echo "Extracting libesedb..."
+	tar xzf libesedb-experimental-20140406.tar.gz
+    rm -f libesedb-experimental-20140406.tar.gz
+	cd libesedb-20140406
+	echo "Building libesedb..."
+	CFLAGS="-g -O2 -Wall -fgnu89-inline" ./configure --enable-static-executables >/dev/null 2>&1
+	make >/dev/null 2>&1
+	cd ..
+	echo "Preparing datafiles using esedbexport..."
+	./libesedb-20140406/esedbtools/esedbexport -t NTDS NTDS/NTDS.dit >/dev/null 2>&1
+    echo "Done! Please execute 'extract.sh' to extract the hashes into the NTDS folder"
+else
+	echo ""
+	echo "Error: NTDS.dit, SYSTEM and SAM are not all present in the NTDS folder"
+	echo ""
+	echo ""
+fi
